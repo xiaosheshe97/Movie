a889b47d606035f46ecd0cfda72a1a1786824d9a
xz222az <xz222az@student.lnu.se>
2022-02-24 15:46:42 +0100

    Update Movie.sql

--- database/Movie.sql
+++ database/Movie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 22, 2022 at 02:31 PM
+-- Generation Time: Feb 24, 2022 at 02:45 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -32,6 +32,24 @@ CREATE TABLE `Actor` (
   `name` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `Actor`
+--
+
+INSERT INTO `Actor` (`id`, `name`) VALUES
+(1, 'Chris Hemsworth'),
+(2, 'Scarlett Johansson'),
+(3, 'Robert Downey Jr.'),
+(4, 'Chris Evans'),
+(5, 'Jeremy Renner'),
+(6, 'Mark Ruffalo'),
+(7, 'Tom Hiddleston'),
+(8, 'Anthony Mackie'),
+(9, 'Tom Holland'),
+(10, 'Benedict Cumberbatch'),
+(11, 'Paul Rudd'),
+(12, 'Chris Pratt');
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +61,18 @@ CREATE TABLE `Director` (
   `name` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `Director`
+--
+
+INSERT INTO `Director` (`id`, `name`) VALUES
+(1, 'Jon Favreau'),
+(2, 'Taika Waititi'),
+(3, 'Kevin Feige'),
+(4, 'Joss Whedon'),
+(5, 'Alan Taylor'),
+(6, 'James Gunn');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +83,19 @@ CREATE TABLE `Genre` (
   `id` int(11) NOT NULL,
   `name` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Genre`
+--
+
+INSERT INTO `Genre` (`id`, `name`) VALUES
+(1, 'Action'),
+(2, 'Fantasy'),
+(3, 'Science Fiction'),
+(4, 'Adventure '),
+(5, 'Comedy '),
+(6, 'Drama'),
+(7, 'Horror ');
 
 -- --------------------------------------------------------
 
@@ -67,6 +110,20 @@ CREATE TABLE `Movie` (
   `year` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `Movie`
+--
+
+INSERT INTO `Movie` (`id`, `name`, `description`, `year`) VALUES
+(1, 'iron man 1', 'Iron Man tells the story of Tony Stark, a billionaire industrialist and genius inventor who is kidnapped and forced to build a devastating weapon. Instead, using his intelligence and ingenuity, Tony builds a high-tech suit of armor and escapes captivity.', '2008'),
+(2, 'Iron Man 2', 'With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father\'s legacy.', '2010'),
+(3, 'Iron Man 3', 'When Tony Stark\'s world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.', '2013'),
+(4, 'Iron Man 4', 'The Movie will start 2 years after the Infinity War. Tony Stark started to make suits again. Now James Rhodes (War Machine) helps him with this. Tony Stark creates the ultimate suit but The Mandarin is against this. He tries to stop Tony Stark with his new ally. Tony also wants his relationship back with Pepper. Also a new threat is coming.', '2019'),
+(5, 'The Avengers ', 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.', '2012'),
+(6, 'Avengers: Age of Ultron', 'When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping program called Ultron, things go horribly wrong and it\'s up to Earth\'s mightiest heroes to stop the villainous Ultron from enacting his terrible plan.', '2015'),
+(7, 'Avengers: Infinity War', 'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.', '2018'),
+(8, 'Avengers: Endgame', 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos\' actions and restore balance to the universe.', '2019');
+
 -- --------------------------------------------------------
 
 --
@@ -77,6 +134,19 @@ CREATE TABLE `Movie_Actor` (
   `movie_id` int(11) NOT NULL,
   `actor_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Movie_Actor`
+--
+
+INSERT INTO `Movie_Actor` (`movie_id`, `actor_id`) VALUES
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 4),
+(5, 5),
+(5, 6),
+(5, 7);
 
 -- --------------------------------------------------------
 
@@ -89,6 +159,13 @@ CREATE TABLE `Movie_Director` (
   `director_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `Movie_Director`
+--
+
+INSERT INTO `Movie_Director` (`movie_id`, `director_id`) VALUES
+(5, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -99,6 +176,17 @@ CREATE TABLE `Movie_Genre` (
   `movie_id` int(11) NOT NULL,
   `genre_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Movie_Genre`
+--
+
+INSERT INTO `Movie_Genre` (`movie_id`, `genre_id`) VALUES
+(5, 1),
+(5, 4),
+(5, 5),
+(5, 2),
+(5, 3);
 
 --
 -- Indexes for dumped tables
