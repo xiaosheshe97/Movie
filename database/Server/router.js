b48791b6017c836aefa1278aef2df59d1a5cebde
xz222az <xz222az@student.lnu.se>
2022-03-26 16:22:34 +0100

    query

--- database/Server/router.js
+++ database/Server/router.js
@@ -4,6 +4,10 @@ const router = express.Router()
 const router_handler = require('./router_handler')
 
 router.get('/Movies',router_handler.getAllMovies)
+router.get('/Moviedir',router_handler.getMoviesNameAndGenre)
+router.get('/MovieActor',router_handler.getMoviesNameAndActor)
+router.get('/MovieGroup',router_handler.getGroup)
+router.get('/MovieView',router_handler.getView)
 
 
 module.exports = router
--- database/Server/router_handler.js
+++ database/Server/router_handler.js
@@ -1,9 +1,73 @@
 const db = require('./db')
 
-exports.getAllMovies = (req,res)=>{
-    const sqlStr = "SELECT * FROM Movie"
-    db.query(sqlStr,(err,results)=>{
-        if(err) return res.send({status:1, message:err.message})
+exports.getAllMovies = (req, res) => {
+    const sqlStr = `
+    SELECT name, description
+    FROM Movie
+    `
+    db.query(sqlStr, (err, results) => {
+        if (err) return res.send({ status: 1, message: err.message })
         res.send(results)
+    })
+}
+
+exports.getMoviesNameAndGenre = (req, res) => {
+    const sqlStr = `
+    SELECT Movie.name AS Movie_name, Genre.name AS Genre_name
+    FROM Movie
+    JOIN Movie_Genre ON Movie.id = Movie_Genre.movie_id 
+    JOIN Genre ON  Movie_Genre.genre_id = Genre.id`
+    db.query(sqlStr, (err, results) => {
+        if (err) return res.send({ status: 1, message: err.message })
+        res.send(results)
+    })
+}
+
+exports.getMoviesNameAndActor = (req, res) => {
+    const sqlStr = `
+    SELECT Movie.name AS Movie_name, Actor.name AS Actor_name,
+    FROM Movie
+    JOIN Movie_Actor ON Movie.id = Movie_Actor.movie_id 
+    JOIN Actor ON  Movie_Actor.actor_id  = Actor.id`
+    db.query(sqlStr, (err, results) => {
+        if (err) return res.send({ status: 1, message: err.message })
+        res.send(results)
+    })
+}
+
+exports.getGroup = (req, res) => {
+    const sqlStr = `
+    SELECT Actor.name AS Actor_name, COUNT(Actor.id) AS Movie_Amount
+    FROM Actor
+    JOIN Movie_Actor ON Actor.id = Movie_Actor.actor_id 
+    GROUP BY Actor.name 
+    `
+    db.query(sqlStr, (err, results) => {
+        if (err) return res.send({ status: 1, message: err.message })
+        res.send(results)
+    })
+}
+
+exports.getView = (req, res) => {
+    
+
+    const sqlStr = `
+    CREATE VIEW Movie_Dir AS
+    SELECT Movie.name, Movie.id
+    FROM Movie
+    WHERE Movie.id < "3"
+    `
+    db.query(sqlStr, (err, results) => {
+        if (err) return res.send({ status: 1, message: "1" })
+    })
+    const sql2 = ` Select * From Movie_Dir
+    `
+    db.query(sql2, (err, results) => {
+        if (err) return res.send({ status: 1, message: err.message })
+        res.send(results)
+    })
+    const sql =`DROP VIEW Movie_Dir`
+    db.query(sql, (err, results) => {
+        if (err) return res.send({ status: 1, message: "3" })
     })
 }
